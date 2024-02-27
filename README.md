529ad70c2d374f2d13ac55f5619726591b96196c
Seth Corker <seth.corker@gmail.com>
2024-02-27 12:07:30 +0000

    output PPM file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Ray Tracing in one weekend
 
 Following https://raytracing.github.io/books/RayTracingInOneWeekend.html to create a ray tracer to learn Zig programming language
+
+Testing output: `zig build run > test.ppm`
---
+++ src/outputter/ppm.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn generate(width: u16, height: u16, max_color: u8) !void {
+    const out = std.io.getStdOut();
+    var bw = std.io.bufferedWriter(out.writer());
+    const stdout = bw.writer();
+
+    try stdout.print("P3\n{} {}\n{}\n", .{ width, height, max_color });
+    const max_color_multiplier = @as(f64, @floatFromInt(max_color));
+
+    for (0..height) |row_index| {
+        for (0..width) |column_index| {
+            const width_offset = @as(f64, @floatFromInt(width - 1));
+            const height_offset = @as(f64, @floatFromInt(height - 1));
+
+            const red = @as(f64, @floatFromInt(column_index)) / width_offset;
+            const green = @as(f64, @floatFromInt(row_index)) / height_offset;
+            const blue = 0;
+
+            const red_channel = @as(u8, @intFromFloat(red * max_color_multiplier));
+            const green_channel = @as(u8, @intFromFloat(green * max_color_multiplier));
+            const blue_channel = @as(u8, @intFromFloat(blue * max_color_multiplier));
+
+            try stdout.print("{} {} {}\n", .{ red_channel, green_channel, blue_channel });
+        }
+    }
+
+    try bw.flush();
+}
